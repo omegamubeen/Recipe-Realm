906d903b3595206340d9037f8bfa223a701402e8
MubeenKhalid <mubeenjutt005@gmail.com>
2024-08-09 21:16:17 +0500

    second commit, designs

--- app/src/main/java/com/app/reciperealm/ui/fragments/CreateRecipeFragment.kt
+++ app/src/main/java/com/app/reciperealm/ui/fragments/CreateRecipeFragment.kt
@@ -7,11 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.app.reciperealm.R
 import com.app.reciperealm.databinding.FragmentCreateRecipeBinding
-import com.app.reciperealm.databinding.FragmentFavoriteBinding
+import com.app.reciperealm.extensions.setVerticalLayout
+import com.app.reciperealm.ui.adapters.IngredientsAdapter
+import com.app.reciperealm.ui.model.CategoryModel
 
 class CreateRecipeFragment : Fragment(R.layout.fragment_create_recipe) {
 
     private var binding: FragmentCreateRecipeBinding? = null
+    private val ingredientsAdapter by lazy { IngredientsAdapter(categroyList) }
+    private val categroyList: ArrayList<CategoryModel> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -23,6 +27,43 @@ class CreateRecipeFragment : Fragment(R.layout.fragment_create_recipe) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        ingredients()
+
+    }
+
+    fun ingredients() {
+        binding!!.rcvIngredients.apply {
+            setVerticalLayout()
+            adapter = ingredientsAdapter
+        }
+        val categroysList = mutableListOf(
+            CategoryModel(
+                "\uD83C\uDF63",
+                "Bread",
+                "200g",
+            ),
+            CategoryModel(
+                "\uD83C\uDF63",
+                "Bread",
+                "200g",
+            ),
+            CategoryModel(
+                "\uD83C\uDF63",
+                "Bread",
+                "200g",
+            ),
+            CategoryModel(
+                "\uD83C\uDF63",
+                "Bread",
+                "200g",
+            ),
+            CategoryModel(
+                "\uD83C\uDF63",
+                "Bread",
+                "200g",
+            ),
+        )
+        binding!!.rcvIngredients.adapter = IngredientsAdapter(categroysList)
     }
 
 }
